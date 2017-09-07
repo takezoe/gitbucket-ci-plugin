757ae6da9bb18c22011171e5555f776c441f6bd3
Naoki Takezoe <takezoe@gmail.com>
2017-09-08 08:32:04 +0900

    (refs #1)Trigger by only commit on the default branch

--- src/main/scala/io/github/gitbucket/ci/hook/SimpleCICommitHook.scala
+++ src/main/scala/io/github/gitbucket/ci/hook/SimpleCICommitHook.scala
@@ -2,17 +2,24 @@ package io.github.gitbucket.ci.hook
 
 import gitbucket.core.plugin.ReceiveHook
 import gitbucket.core.model.Profile._
+import gitbucket.core.service.{AccountService, RepositoryService}
 import io.github.gitbucket.ci.service.{BuildSetting, SimpleCIService}
 import org.eclipse.jgit.transport.{ReceiveCommand, ReceivePack}
 import profile.api._
 
 
-class SimpleCICommitHook extends ReceiveHook with SimpleCIService {
+class SimpleCICommitHook extends ReceiveHook with SimpleCIService with RepositoryService with AccountService {
 
   override def postReceive(owner: String, repository: String, receivePack: ReceivePack, command: ReceiveCommand, pusher: String)
                           (implicit session: Session): Unit = {
-    // TODO Don't run a build if the pushed commit is not for the default branch.
-    runBuild(owner, repository, command.getNewId.name, BuildSetting(owner, repository, "sbt compile"))
+    val branch = command.getRefName.stripPrefix("refs/heads/")
+    if(branch != command.getRefName){
+      getRepository(owner, repository).foreach { repositoryInfo =>
+        if(repositoryInfo.repository.defaultBranch == branch){
+          runBuild(owner, repository, command.getNewId.name, BuildSetting(owner, repository, "sbt compile"))
+        }
+      }
+    }
   }
 
 }
